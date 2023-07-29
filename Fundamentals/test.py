ecb52937b5bfd63eaf49c14d14b02b4e18eeb5df
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-07-29 17:20:42 +0300

    v9

--- Fundamentals/test.py
+++ Fundamentals/test.py
@@ -1,4 +1,27 @@
 import torch
 
+# Reproducibility (trying to take random out of random)
+
+# Create two random tensors
+random_tensor_A = torch.rand(3, 4)
+random_tensor_B = torch.rand(3, 4)
+
+print(random_tensor_A)
+print(random_tensor_B)
+print(random_tensor_A == random_tensor_B)
 
 
+# Let's make some random but reproducible tensors
+
+# Set the random seed
+RANDOM_SEED = 42
+
+torch.manual_seed(RANDOM_SEED)
+random_tensor_C = torch.rand(3, 4)
+
+torch.manual_seed(RANDOM_SEED)
+random_tensor_D = torch.rand(3, 4)
+
+print(random_tensor_C)
+print(random_tensor_D)
+print(random_tensor_C == random_tensor_D)
