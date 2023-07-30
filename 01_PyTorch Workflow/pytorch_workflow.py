c91d33758f8ecbb31316dd429c563af37d5d8b6e
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-07-30 10:29:04 +0300

    v13

--- 01_PyTorch Workflow/pytorch_workflow.py
+++ 01_PyTorch Workflow/pytorch_workflow.py
@@ -19,3 +19,42 @@ y = weight * X + bias
 
 print(X[:10], y[:10])
 print(len(X), len(y))
+
+## Splitting data into training and tests sets (one of the most important concepts in machine learning in general)
+
+# Create a train/test split
+train_split = int(0.8 * len(X))
+print(train_split)
+
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+## Visualizing data
+
+def plot_predictions(train_data = X_train,
+                     train_labels = y_train,
+                     test_data = X_test,
+                     test_labels = y_test,
+                     predictions = None):
+    """
+    Plots training data, test data and compares predictions.
+    """
+    plt.figure(figsize=(10, 7))
+
+    # Plot training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+
+    # Plot test data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    # Are there predictions?
+    if predictions is not None:
+        # Plot the predictions if they exist
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # Show the legend
+    plt.legend(prop={"size": 14});
+
+
