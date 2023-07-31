b9a37b38d13dc255c78940381e6c5bdcf0c70977
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-07-31 21:13:13 +0300

    v26

--- 01_PyTorch Workflow/test.py
+++ 01_PyTorch Workflow/test.py
@@ -9,5 +9,63 @@ import matplotlib.pyplot as plt
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
+### 1. Data
+
+# Create some data using the linear regression formula of y = weight * X + bias
+weight = 0.7
+bias = 0.3
+
+# Create range values
+start = 0
+end = 1
+step = 0.02
+
+# Create X and y (features and labels)
+X = torch.arange(start, end, step). unsqueeze(dim=1) # without unsqueeze, errors will pop up
+y = weight * X + bias
+print("X and y values:")
+print(X[:10], y[:10])
+
+# Split data
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+print("Samples must be 40 training samples and 10 testing samples.")
+print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+# Plot the data
+
+# Note: if you don't have the plot_predictions() function loaded, this will give an error
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+    """
+    Plots training data, test data and compares predictions.
+    """
+    plt.figure(figsize=(10, 7))
+
+    # Plot training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    
+    # Plot test data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        # Plot the predictions in red (predictions were made on the test data)
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # Show the legend
+    plt.legend(prop={"size": 14})
+    plt.show()
+
+plot_predictions(X_train, y_train, X_test, y_test)
+
+
+
+### 2. Building a PyTorch Linear Model
+
+
 
 
