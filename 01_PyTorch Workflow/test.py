f1b85e82ce095f42628eb795e6353e6ed2ce3964
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-07-31 19:16:03 +0300

    v25

--- 01_PyTorch Workflow/test.py
+++ 01_PyTorch Workflow/test.py
@@ -1,4 +1,13 @@
+## Putting all together
+
+# Import PyTorch and matplotlib
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
+
+# Setup device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
 
