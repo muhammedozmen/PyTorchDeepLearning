623b72510f209fda513cd02eff0d10b603dee630
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-08-01 19:30:51 +0300

    v30

---
+++ 02_PyTorch Classification/pytorch_classification.py
@@ -0,0 +1,32 @@
+## 1. Make classification data and get it ready
+
+import sklearn
+from sklearn.datasets import make_circles
+
+# Make 1000 samples
+n_samples = 1000
+
+# Create circles
+X, y = make_circles(n_samples, noise=0.03, random_state=42)
+
+print(len(X), len(y))
+
+print(f"First 5 samples of X:\n {X[:5]}")
+print(f"First 5 samples of y:\n {y[:5]}")
+
+# Make DataFrame of circle data
+import pandas as pd
+circles = pd.DataFrame({"X1": X[:, 0],
+                        "X2": X[:, 1],
+                        "label": y
+                        })
+print(circles.head(10))
+
+# Visualizing data
+import matplotlib.pyplot as plt
+plt.scatter(x=X[:, 0],
+            y=X[:, 1],
+            c=y,
+            cmap=plt.cm.RdYlBu)
+plt.show()
+
