fd20a68c004817b9f927c650cf5e51bdf9b65173
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-08-28 20:27:06 +0300

    v51

---
+++ 03_PyTorch Computer Vision/pytorch_computer_vision.py
@@ -0,0 +1,13 @@
+# Import PyTorch
+import torch
+from torch import nn
+
+# Import torchvision
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+
