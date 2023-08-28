80f021a17d121570df6d3cfd175292c2d0e0d966
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-08-28 20:49:18 +0300

    v52

--- 03_PyTorch Computer Vision/pytorch_computer_vision.py
+++ 03_PyTorch Computer Vision/pytorch_computer_vision.py
@@ -11,3 +11,42 @@ from torchvision.transforms import ToTensor
 # Import matplotlib for visualization
 import matplotlib.pyplot as plt
 
+
+
+## 1. Getting a dataset
+
+# Setup training data
+from torchvision import datasets
+train_data = datasets.FashionMNIST(
+    root="data", # where to download data to
+    train=True, # do we want the training dataset
+    download=True, # do we want to download
+    transform= torchvision.transforms.ToTensor(), # how do we want to transform the data
+    target_transform=None # how do we want to transform the labels/targets
+)
+
+test_data = datasets.FashionMNIST(
+    root="data", 
+    train=False, 
+    download=True, 
+    transform= torchvision.transforms.ToTensor(), 
+    target_transform=None 
+)
+
+print(len(train_data), len(test_data))
+
+# See the first training example
+image, label = train_data[0]
+print(image, label)
+
+class_names = train_data.classes
+print(class_names)
+
+class_to_idx = train_data.class_to_idx
+print(class_to_idx)
+
+print(train_data.targets)
+
+# Check the shape of our image
+print(f"Image shape: {image.shape} -> [color_channels, height, width]")
+print(f"Image label: {class_names[label]}")
