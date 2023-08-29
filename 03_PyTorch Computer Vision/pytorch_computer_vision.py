639c04d830961991b2d2172f3a7bcf086e0896ff
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-08-29 12:23:16 +0300

    v53

--- 03_PyTorch Computer Vision/pytorch_computer_vision.py
+++ 03_PyTorch Computer Vision/pytorch_computer_vision.py
@@ -47,6 +47,42 @@ print(class_to_idx)
 
 print(train_data.targets)
 
+
+
+## 1.1 Check input and output shapes of data
+
 # Check the shape of our image
 print(f"Image shape: {image.shape} -> [color_channels, height, width]")
 print(f"Image label: {class_names[label]}")
+
+
+
+## 1.2 Visualizing our data
+import matplotlib.pyplot as plt
+
+image, label = train_data[0]
+print(f"Image shape: {image.shape}")
+
+plt.imshow(image.squeeze())
+plt.title(label)
+plt.show()
+
+plt.imshow(image.squeeze(), cmap="gray")
+plt.title(class_names[label])
+plt.axis(False)
+plt.show()
+
+# Plot more images
+torch.manual_seed(42)
+fig = plt.figure(figsize=(9, 9))
+rows, cols = 4, 4
+for i in range(1, rows*cols+1):
+    random_idx = torch.randint(0, len(train_data), size=[1]).item()
+    img, label = train_data[random_idx]
+    fig.add_subplot(rows, cols, i)
+    plt.imshow(img.squeeze(), cmap="gray")
+    plt.title(class_names[label])
+    plt.axis(False)
+    plt.tight_layout()
+    
+plt.show()
