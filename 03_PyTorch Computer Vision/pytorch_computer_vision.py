4bed2a10bb42aca075c190474c5afe5a03ee010a
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-08-30 14:59:53 +0300

    v58

--- 03_PyTorch Computer Vision/pytorch_computer_vision.py
+++ 03_PyTorch Computer Vision/pytorch_computer_vision.py
@@ -198,3 +198,15 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(params=model_0.parameters(),
                             lr=0.1)
 
+
+## 3.2 Creating a function to time our experiments
+from timeit import default_timer as timer
+
+def print_train_time(start: float,
+                     end: float,
+                     device: torch.device = None):
+    """Prints difference between start and end time"""
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
+
